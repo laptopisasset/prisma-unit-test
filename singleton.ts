f9ec5b3d2eb5204cb53625fe5380649642b64dc0
Soundhar <laptopisasset@gmail.com>
2021-05-16 18:14:02 +0530

    Added the unit test elements.

---
+++ singleton.ts
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@prisma/client";
+import { mockDeep, mockReset, MockProxy } from "jest-mock-extended";
+
+import prisma from "./client";
+
+jest.mock("./client", () => ({
+  __esModule: true,
+  default: mockDeep<PrismaClient>(),
+}));
+
+export const prismaMock = prisma as unknown as MockProxy<PrismaClient>;
